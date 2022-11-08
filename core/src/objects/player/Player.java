60d826a687ed7af2429e30ad36bbdd6a13588551
Luqman Patel <luqman.patel@gmail.com>
2022-11-08 00:00:38 -0500

    re-org files

--- core/src/objects/player/Player.java
+++ core/src/objects/player/Player.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 
 import helper.Constants;
-import objects.GameEntity;
 
 public class Player extends GameEntity {
 
