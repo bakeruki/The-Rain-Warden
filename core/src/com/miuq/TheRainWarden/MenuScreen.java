c7e6fcf567174e7149224b69df28ad4cad178da6
Luqman Patel <luqman.patel@gmail.com>
2022-11-30 19:28:29 -0500

    better placeholder animations and fade in effects

--- core/src/com/miuq/TheRainWarden/MenuScreen.java
+++ core/src/com/miuq/TheRainWarden/MenuScreen.java
@@ -6,23 +6,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Cursor.SystemCursor;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.miuq.animation.Animation;
 
 
 public class MenuScreen extends ScreenAdapter{
-    private SpriteBatch batch;
-    private Animation backgroundAnimation;
-
     private TheRainWarden game;
 
     private ImageButton startButton;
@@ -36,6 +33,8 @@ public class MenuScreen extends ScreenAdapter{
     private ImageButtonStyle startStyle;
     private ImageButtonStyle exitStyle;
 
+    private Image backgroundImage;
+
     private Stage stage;
     // private GameScreen gameScreen;
     private MenuScreen menuScreen;
@@ -43,8 +42,6 @@ public class MenuScreen extends ScreenAdapter{
 
     public MenuScreen(OrthographicCamera camera, FitViewport viewport, TheRainWarden game){
         camera.setToOrtho(false, 0, 0);
-        this.batch = new SpriteBatch();
-        this.backgroundAnimation = new Animation(new TextureRegion(new Texture("assets/menuBackground.png")), 13, 2.1f);
 
         this.startDrawable = new TextureRegionDrawable(new TextureRegion(new Texture("assets/buttons/startButton/start.png")));
         this.startDownDrawable = new TextureRegionDrawable(new TextureRegion(new Texture("assets/buttons/startButton/startDown.png")));
@@ -62,15 +59,19 @@ public class MenuScreen extends ScreenAdapter{
         this.exitButton = new ImageButton(exitStyle);
         this.exitButton.setPosition(startButton.getX(), startButton.getY() - 150);
 
+        this.backgroundImage = new Image(new Texture("assets/menuBackground.png"));
+
         this.game = game;
-        // this.gameScreen = new GameScreen(camera, viewport, game);
+        // this.gameScreen = new GameScreen(camera, viewport, game); leave this here in case we get annoyed with watching the cutscene every time
         this.menuScreen = this;
         this.cutsceneScreen = new CutsceneScreen(camera, viewport, game);
 
         this.stage = new Stage(viewport);
 
+        this.stage.addActor(backgroundImage);
         this.stage.addActor(startButton); 
         this.stage.addActor(exitButton);
+        this.stage.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(1)));
 
         Gdx.input.setInputProcessor(stage);
         Gdx.graphics.setSystemCursor(SystemCursor.Arrow);
@@ -81,22 +82,20 @@ public class MenuScreen extends ScreenAdapter{
     public void render(float delta){
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        backgroundAnimation.update(delta);
-        
-        batch.begin();
-        batch.draw(backgroundAnimation.getFrame(), 0, 30);
-        batch.end();
         
         startButton.center();
         startButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y){
-                game.setScreen(cutsceneScreen);
-                Gdx.input.setInputProcessor(cutsceneScreen.getStage());
-                stage.dispose();
-                batch.dispose();
-                menuScreen.dispose();
+                stage.addAction(Actions.sequence(Actions.alpha(1), Actions.fadeOut(2), Actions.run(new Runnable(){
+                    @Override
+                    public void run(){
+                        game.setScreen(cutsceneScreen);
+                    Gdx.input.setInputProcessor(cutsceneScreen.getStage());
+                    stage.dispose();
+                    menuScreen.dispose();
+                    }
+                })));
             }
         });
         
